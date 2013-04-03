ce6eb96569815b5eed02294b0fd32b2df67192b1
Kris Lange <klange@kris-2.local>
2013-04-03 14:43:41 -0400

    new method: add_multiple_columns()

--- lib/cassandra/0.8/cassandra.rb
+++ lib/cassandra/0.8/cassandra.rb
@@ -22,4 +22,30 @@ class Cassandra
 
     @batch ? @batch << [mutation_map, options[:consistency]] : _mutate(mutation_map, options[:consistency])
   end
+
+  # Increment one or more counters in a single row.
+  def add_multiple_columns(column_family, key, hash, options = {})
+    column_family, _, _, options = extract_and_validate_params(column_family, key, [options], WRITE_DEFAULTS)
+
+    mutation_map = if is_super(column_family)
+      {
+        key => {
+          column_family => hash.collect do |column, sub_hash|
+            sub_hash.collect do |sub_column, value|
+              _super_counter_mutation(column_family, column, sub_column, value)
+            end
+          end.flatten
+        }
+      }
+    else
+      {
+        key => {
+          column_family => hash.collect { |column, value| _standard_counter_mutation(column_family, column, value) }
+        }
+      }
+    end
+
+    @batch ? @batch << [mutation_map, options[:consistency]] : _mutate(mutation_map, options[:consistency])
+  end
+
 end
